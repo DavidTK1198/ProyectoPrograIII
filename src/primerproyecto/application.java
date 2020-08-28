ef6a4fc1bbc1e4e014157de18b2d02d66b86a0d4
DavidTK1198 <70339682+DavidTK1198@users.noreply.github.com>
2020-08-28 11:49:33 -0600

    Hola mundo

--- src/primerproyecto/application.java
+++ src/primerproyecto/application.java
@@ -16,8 +16,10 @@ public class application {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hola mundo");
     }
     
 }
+
 
 
