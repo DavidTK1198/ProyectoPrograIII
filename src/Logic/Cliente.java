d200e474a00f3007b04880011c51e14d8bd96ba4
DavidTK1198 <70339682+DavidTK1198@users.noreply.github.com>
2020-08-30 11:14:28 -0600

    Se agrego la clase cliente

---
+++ src/Logic/Cliente.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author DavidTK1198
+ */
+public class Cliente {
+
+    private String nombre;
+    private String cedula;
+    private String CorreoE;
+
+    public Cliente() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreoE() {
+        return CorreoE;
+    }
+
+    public void setCorreoE(String CorreoE) {
+        this.CorreoE = CorreoE;
+    }
+    
+
+}
