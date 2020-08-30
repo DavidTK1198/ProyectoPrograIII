d6d546d2c3a7530c445d148250ae4a1d272fee26
DavidTK1198 <70339682+DavidTK1198@users.noreply.github.com>
2020-08-30 11:17:30 -0600

    actualizacion

---
+++ src/Application/Application.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Application;
+
+/**
+ *
+ * @author DavidTK1198
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code Application logic here
+        System.out.println("Hola mundo");
+    }
+    
+}
+
+
+
