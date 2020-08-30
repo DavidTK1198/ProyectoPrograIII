ed1c26c7ce1d514db2de6deb5f25a94e39a6e143
DavidTK1198 <70339682+DavidTK1198@users.noreply.github.com>
2020-08-30 11:14:15 -0600

    Se agrego la clase Empresa

---
+++ src/Logic/Empresa.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+/**
+ *
+ * @author DavidTK1198
+ */
+public class Empresa {
+
+    private static Empresa single_instance = null; //Singleton
+    private String nombre;
+    private String telefono;
+    private String correoE;
+    private String Cedula;
+    private String direccion;
+
+    private Empresa() {
+    }
+
+    public static Empresa getInstance() {
+        if (single_instance == null) {
+            single_instance = new Empresa();
+        }
+
+        return single_instance;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreoE() {
+        return correoE;
+    }
+
+    public void setCorreoE(String correoE) {
+        this.correoE = correoE;
+    }
+
+    public String getCedula() {
+        return Cedula;
+    }
+
+    public void setCedula(String Cedula) {
+        this.Cedula = Cedula;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+
+}
