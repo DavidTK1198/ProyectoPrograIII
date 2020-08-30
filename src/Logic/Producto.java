480dc664a233ec296f1abe4ab306fbb8a231bd1d
DanielMadrigalVasquez <dan1998ctpu@gmail.com>
2020-08-30 12:59:55 -0600

    Agregué la clase producto

---
+++ src/Logic/Producto.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Logic;
+
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Daniel Madrigal
+ */
+public class Producto {
+    private String codigo;
+    private String descripcion;
+    private double precioUnitario;
+
+    public Producto(String codigo , String descripcion, double precioUnitario) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public double getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setPrecioUnitario(double precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "codigo=" + codigo + ", descripcion=" + descripcion + ", precioUnitario=" + precioUnitario + '}';
+    }
+    
+}
